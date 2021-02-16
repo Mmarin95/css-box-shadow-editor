66083f45e75fec293b8e15534533239ca5e13b15
Miquel Marin <mmarin_79@hotmail.com>
2021-02-16 19:01:10 +0100

    add copy button text

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let DEFAULTS = [];
+let DEFAULTS = {};
+
+const cssPropertyOutput = document.querySelector("[data-css-property]");
+const buttonCopyText = document.querySelector("[data-copy-text]");
 
 const dataInputsConfiguration = document.querySelectorAll(
   "[data-input-configuration]"
@@ -16,6 +19,7 @@ dataInputsConfiguration.forEach((input) => {
   };
 
   input.addEventListener("input", inputChanged);
+  displayCSSRule();
 });
 
 document.querySelector("[data-button-reset]").addEventListener("click", reset);
@@ -25,6 +29,7 @@ function inputChanged(e) {
     `--${e.target.name}`,
     e.target.value
   );
+  displayCSSRule();
 }
 
 function reset() {
@@ -36,4 +41,28 @@ function reset() {
       DEFAULTS[input.name]
     );
   });
+  displayCSSRule();
+}
+
+function displayCSSRule() {
+  let rules = {};
+
+  dataInputsConfiguration.forEach((input) => {
+    rules = {
+      ...rules,
+      [input.name]: input.value,
+    };
+  });
+
+  const rule = `box-shadow: ${rules["x-position"]} ${rules["y-position"]} ${rules["blur"]} ${rules["spread"]} ${rules["shadow-color"]}`;
+
+  cssPropertyOutput.value = rule;
+}
+
+
+buttonCopyText.addEventListener("click", copyText);
+function copyText() {
+  cssPropertyOutput.select();
+  cssPropertyOutput.setSelectionRange(0, 99999);
+  document.execCommand("copy");
 }
